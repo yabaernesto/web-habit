57abf29ea9ffb5c4e3b9b269fc00667cbf96ecb4
Yaba Ernesto <yabaernesto@gmail.com>
2026-07-23 20:22:10 +0100

    feat: header adicionado

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,11 @@
-import { Habit } from "./components/habit";
+import { Header } from "./components/header";
 
 export function App() {
   return (
-    <div>
-      <Habit completed={2} />
-      <Habit completed={3} />
-      <Habit completed={6} />
-      <Habit completed={10} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
     </div>
   );
 }
